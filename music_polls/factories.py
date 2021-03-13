460fefa8e58855a8445c101630fb9eda465d1d6f
Serezha Kharitonchuk <sergeyk@improvado.io>
2021-03-13 15:08:34 +0300

    add todo, fix mgmail typo

--- music_polls/factories.py
+++ music_polls/factories.py
@@ -2,6 +2,7 @@ import factory
 from django.contrib.auth.models import User
 
 
+# todo: move UserFactory to single application
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
@@ -9,6 +10,5 @@ class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: f'username_{n}@gmail.com')
     first_name = factory.Sequence(lambda n: f'fist_name_{n}@gmail.com')
     last_name = factory.Sequence(lambda n: f'last_name_{n}@gmail.com')
-    email = factory.Sequence(lambda n: f'fake_email_{n}@mgmail.com')
+    email = factory.Sequence(lambda n: f'fake_email_{n}@gmail.com')
     password = 'example password'
-
