cfedd558c12cd9bc56c1ced495215cad34bc40a1
Serezha Kharitonchuk <sergeyk@improvado.io>
2021-03-13 22:38:16 +0300

    set default permission class (IsAuthenticated) to DRF settings

--- music_polls/settings.py
+++ music_polls/settings.py
@@ -128,5 +128,8 @@ STATIC_ROOT = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
 }
